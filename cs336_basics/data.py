59376943dbf72b67c4d0c2384fee30385c8888d9
julyfun-alicluster2 <julyfun.collect@outlook.com>
2026-04-28 10:19:59 +0800

    Problem (checkpointing): Implement model checkpointing (1 point)

--- cs336_basics/data.py
+++ cs336_basics/data.py
@@ -3,6 +3,8 @@ import numpy.typing as npt
 import torch
 from torch import Tensor
 import random
+import os
+import typing
 
 def get_batch(
     dataset: npt.NDArray, batch_size: int, context_length: int, device: str
@@ -15,4 +17,29 @@ def get_batch(
         xs.append(torch.tensor(dataset[st:st + context_length]))
         ys.append(torch.tensor(dataset[st + 1:st + context_length + 1]))
     return torch.stack(xs).to(device), torch.stack(ys).to(device)
- 
+    
+def save_checkpoint(
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    iteration: int,
+    out: str | os.PathLike | typing.BinaryIO | typing.IO[bytes],
+):
+    torch.save(
+        {
+            "model": model.state_dict(),
+            "optimizer": optimizer.state_dict(),
+            "iteration": iteration,
+        },
+        out,
+    )
+    
+    
+def load_checkpoint(
+    src: str | os.PathLike | typing.BinaryIO | typing.IO[bytes],
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+) -> int:
+    d = torch.load(src)
+    model.load_state_dict(d["model"])
+    optimizer.load_state_dict(d["optimizer"])
+    return d["iteration"]
--- tests/adapters.py
+++ tests/adapters.py
@@ -582,7 +582,7 @@ def run_save_checkpoint(
             we've completed.
         out (str | os.PathLike | BinaryIO | IO[bytes]): Path or file-like object to serialize the model, optimizer, and iteration to.
     """
-    raise NotImplementedError
+    data.save_checkpoint(model, optimizer, iteration, out)
 
 
 def run_load_checkpoint(
@@ -603,7 +603,7 @@ def run_load_checkpoint(
     Returns:
         int: the previously-serialized number of iterations.
     """
-    raise NotImplementedError
+    return data.load_checkpoint(src, model, optimizer)
 
 
 def get_tokenizer(
